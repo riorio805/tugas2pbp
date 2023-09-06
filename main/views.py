ad0ca813a65ee185b35df126bb7e9abc8b28f2a0
riorio805 <sefrianojieftara@gmail.com>
2023-09-06 13:37:56 +0700

    main things done

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from main.models import Item
 
 # Create your views here.
+def show_main(request):
+    items = Item.objects.all().values()
+    context = {
+        'items':items,
+    }
+
+    return render(request, "main.html", context)
