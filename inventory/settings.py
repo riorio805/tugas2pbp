0340a0d8ffdb0ae15d6caa4f2ec860209b4c1a0a
riorio805 <sefrianojieftara@gmail.com>
2023-09-09 16:34:17 +0700

    debugging

--- inventory/settings.py
+++ inventory/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v$ihck!00owdcdw3s6@7uv@tn0wuc#q3qcpm_j(w0@(vj(2u9e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
     'main',
 ]
