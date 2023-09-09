a60fcfef8fc00b02a38555da8cc0a83ac9bdbf02
riorio805 <sefrianojieftara@gmail.com>
2023-09-09 16:40:04 +0700

    try2

--- inventory/settings.py
+++ inventory/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
